cd23bf69b3e307f53a527531e2e682e0ff9341be
Gerald <geraldpng@gmail.com>
2019-12-05 14:34:18 +0800

    Add day 5

---
+++ day5/main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func timer(message string) func() {
+	start := time.Now()
+	return func() { fmt.Println(message, ": ", time.Since(start)) }
+}
+
+func main() {
+	log.Println("Running day 5")
+	defer timer("day 5 total")()
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	text := ""
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		text += line
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	intcodeStrings := strings.Split(text, ",")
+	intcodes := []int{}
+
+	for _, v := range intcodeStrings {
+		intcode, err := strconv.Atoi(v)
+		if err != nil {
+			log.Printf("Failed to convert text to integer\nline: %s\nerror: %v", v, err)
+		}
+		intcodes = append(intcodes, intcode)
+	}
+
+	diagnosticCode := alg(intcodes, 1)
+	log.Printf("Part 1: Final value: %d", diagnosticCode)
+	diagnosticCode2 := alg2(intcodes, 5)
+	log.Printf("Part 2: Final value: %d", diagnosticCode2)
+
+}
+
+func alg(intcodes []int, input int) int {
+	defer timer("program1")()
+	// copy slice to prevent modifying original instructions
+	codes := make([]int, len(intcodes))
+	copy(codes, intcodes)
+	pos := 0
+	lastOutput := 0
+	for codes[pos] != 99 {
+		instr := splitInt(codes[pos])
+		opCode := instr[0]
+		if opCode == 1 { // add
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			codes[codes[pos+3]] = param1 + param2
+			pos += 4
+		}
+		if opCode == 2 { // multiply
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			codes[codes[pos+3]] = param1 * param2
+			pos += 4
+		}
+		if opCode == 3 { // set input
+			codes[codes[pos+1]] = input
+			pos += 2
+		}
+		if opCode == 4 { // output
+			lastOutput = codes[codes[pos+1]]
+			pos += 2
+		}
+	}
+	return lastOutput
+}
+
+func alg2(intcodes []int, input int) int {
+	defer timer("program2")()
+	// copy slice to prevent modifying original instructions
+	codes := make([]int, len(intcodes))
+	copy(codes, intcodes)
+	pos := 0
+	lastOutput := 0
+	for codes[pos] != 99 {
+		instr := splitInt(codes[pos])
+		opCode := instr[0]
+		if opCode == 1 { // add
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			param3 := codes[pos+3]
+			codes[param3] = param1 + param2
+			if param3 != pos {
+				pos += 4
+			}
+		}
+		if opCode == 2 { // multiply
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			param3 := codes[pos+3]
+			codes[param3] = param1 * param2
+			if param3 != pos {
+				pos += 4
+			}
+		}
+		if opCode == 3 { // set input
+			codes[codes[pos+1]] = input
+			pos += 2
+		}
+		if opCode == 4 { // output
+			param1 := paramValue(codes, instr, pos, 1)
+			lastOutput = param1
+			pos += 2
+		}
+		if opCode == 5 { // jump if true
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			if param1 > 0 {
+				pos = param2
+			} else {
+				pos += 3
+			}
+		}
+		if opCode == 6 { // jump if false
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			if param1 == 0 {
+				pos = param2
+			} else {
+				pos += 3
+			}
+		}
+		if opCode == 7 { // less than
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			param3 := codes[pos+3]
+			if param1 < param2 {
+				codes[param3] = 1
+			} else {
+				codes[param3] = 0
+			}
+			if param3 != pos {
+				pos += 4
+			}
+		}
+		if opCode == 8 { // equals
+			param1 := paramValue(codes, instr, pos, 1)
+			param2 := paramValue(codes, instr, pos, 2)
+			param3 := codes[pos+3]
+			if param1 == param2 {
+				codes[param3] = 1
+			} else {
+				codes[param3] = 0
+			}
+			if param3 != pos {
+				pos += 4
+			}
+		}
+	}
+	return lastOutput
+}
+
+func splitInt(input int) []int {
+	if input < 10 {
+		return []int{input}
+	}
+	arr := []int{}
+	for i := 1; i < input; i *= 10 {
+		if i == 10 {
+			continue
+		}
+		arr = append(arr, (input%(i*10))/i)
+	}
+	return arr
+}
+
+func paramValue(codes []int, instr []int, pos int, index int) int {
+	mode := 0
+	if len(instr) > index {
+		mode = instr[index]
+	}
+	if mode == 1 {
+		return codes[pos+index]
+	}
+	return codes[codes[pos+index]]
+}
---
+++ day5/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestProgram(t *testing.T) {
+	testCases := []struct {
+		opCodes []int
+		input   int
+		result  int
+	}{
+		{opCodes: []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, input: 8, result: 1},
+		{opCodes: []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, input: 7, result: 1},
+		{opCodes: []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}, input: 8, result: 1},
+		{opCodes: []int{3, 3, 1107, -1, 8, 3, 4, 3, 99}, input: 7, result: 1},
+		{opCodes: []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+			1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+			999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}, input: 7, result: 999},
+		{opCodes: []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+			1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+			999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}, input: 8, result: 1000},
+		{opCodes: []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+			1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+			999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}, input: 9, result: 1001},
+	}
+
+	for _, tc := range testCases {
+		result := alg2(tc.opCodes, tc.input)
+		if tc.result != result {
+			t.Errorf("Expected %d but got %d", tc.result, result)
+		}
+	}
+}
